04e3518319f5681f48c014b6a5abf1bfddb5ff3d
pgswinter <tuannanh.nguyen.freelance@gmail.com>
2020-02-08 17:22:19 +0700

    2nd commit

--- index.js
+++ index.js
@@ -13,3 +13,21 @@ window.addEventListener("appinstalled", evt => {
 if (window.navigator.standalone === true) {
   console.log("display-mode is standalone");
 }
+
+const btnAdd = document.querySelector(".btnAdd");
+
+btnAdd.addEventListener("click", e => {
+  // hide our user interface that shows our A2HS button
+  btnAdd.style.display = "none";
+  // Show the prompt
+  deferredPrompt.prompt();
+  // Wait for the user to respond to the prompt
+  deferredPrompt.userChoice.then(choiceResult => {
+    if (choiceResult.outcome === "accepted") {
+      console.log("User accepted the A2HS prompt");
+    } else {
+      console.log("User dismissed the A2HS prompt");
+    }
+    deferredPrompt = null;
+  });
+});
