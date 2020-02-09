30c530794106999e6080ad5ae18040101534e659
pgswinter <tuannanh.nguyen.freelance@gmail.com>
2020-02-09 19:18:27 +0700

    4th

--- scripts/install.js
+++ scripts/install.js
@@ -1,12 +1,10 @@
 let deferredPrompt;
 
 window.addEventListener("beforeinstallprompt", e => {
-  if (deferredPrompt) {
-    // Stash the event so it can be triggered later.
-    deferredPrompt = e;
-    // Update UI notify the user they can add to home screen
-    showInstallPromotion();
-  }
+  // Stash the event so it can be triggered later.
+  deferredPrompt = e;
+  // Update UI notify the user they can add to home screen
+  showInstallPromotion();
 });
 
 window.addEventListener("appinstalled", evt => {
@@ -21,18 +19,20 @@ const btnAdd = document.getEl(".btnAdd");
 
 btnAdd.addEventListener("click", e => {
   // hide our user interface that shows our A2HS button
-  btnAdd.style.display = "none";
-  // Show the prompt
-  deferredPrompt.prompt();
-  // Wait for the user to respond to the prompt
-  deferredPrompt.userChoice.then(choiceResult => {
-    if (choiceResult.outcome === "accepted") {
-      console.log("User accepted the A2HS prompt");
-    } else {
-      console.log("User dismissed the A2HS prompt");
-    }
-    deferredPrompt = null;
-  });
+  if (deferredPrompt) {
+    btnAdd.style.display = "none";
+    // Show the prompt
+    deferredPrompt.prompt();
+    // Wait for the user to respond to the prompt
+    deferredPrompt.userChoice.then(choiceResult => {
+      if (choiceResult.outcome === "accepted") {
+        console.log("User accepted the A2HS prompt");
+      } else {
+        console.log("User dismissed the A2HS prompt");
+      }
+      deferredPrompt = null;
+    });
+  }
 });
 
 // let deferredInstallPrompt = null;
