09bbc0ee1a8584caa0f6bc4e468b6b26b52c3e2c
pgswinter <tuannanh.nguyen.freelance@gmail.com>
2020-02-09 19:10:20 +0700

    4th

--- scripts/install.js
+++ scripts/install.js
@@ -1,70 +1,79 @@
-let deferredInstallPrompt = null;
-const installButton = document.getElementById("btnAdd");
-installButton.addEventListener("click", installPWA);
+let deferredPrompt;
 
-window.addEventListener("beforeinstallprompt", saveBeforeInstallPromptEvent);
+window.addEventListener("beforeinstallprompt", e => {
+  // Stash the event so it can be triggered later.
+  deferredPrompt = e;
+  // Update UI notify the user they can add to home screen
+  showInstallPromotion();
+});
 
-function saveBeforeInstallPromptEvent(evt) {
-  // CODELAB: Add code to save event & show the install button.
-  deferredInstallPrompt = evt;
-  installButton.removeAttribute("hidden");
+const btnAdd = document.getElementById("btnAdd");
+
+btnAdd.addEventListener("click", e => {
+  // hide our user interface that shows our A2HS button
+  btnAdd.style.display = "none";
+  // Show the prompt
+  deferredPrompt.prompt();
+  // Wait for the user to respond to the prompt
+  deferredPrompt.userChoice.then(choiceResult => {
+    if (choiceResult.outcome === "accepted") {
+      console.log("User accepted the A2HS prompt");
+    } else {
+      console.log("User dismissed the A2HS prompt");
+    }
+    deferredPrompt = null;
+  });
+});
+
+window.addEventListener("appinstalled", evt => {
+  console.log("a2hs installed");
+});
+
+if (window.navigator.standalone === true) {
+  console.log("display-mode is standalone");
 }
 
-function installPWA(evt) {
-  if (deferredInstallPrompt !== undefined) {
-    // CODELAB: Add code show install prompt & hide the install button.
-    deferredInstallPrompt.prompt();
-    // Hide the install button, it can't be called twice.
-    evt.srcElement.setAttribute("hidden", true);
-    // CODELAB: Log user response to prompt.
-    deferredInstallPrompt.userChoice.then(choice => {
-      if (choice.outcome === "accepted") {
-        console.log("User accepted the A2HS prompt", choice);
-      } else {
-        console.log("User dismissed the A2HS prompt", choice);
-      }
-      deferredInstallPrompt = null;
-    });
-    deferredInstallPrompt.userChoice.then(choice => {
-      if (choice.outcome === "accepted") {
-        console.log("User accepted the A2HS prompt", choice);
-      } else {
-        console.log("User dismissed the A2HS prompt", choice);
-      }
-      deferredInstallPrompt = null;
-    });
-  }
-}
+// let deferredInstallPrompt = null;
+// const installButton = document.getElementById("btnAdd");
+// installButton.addEventListener("click", installPWA);
 
-window.addEventListener("appinstalled", logAppInstalled);
+// window.addEventListener("beforeinstallprompt", saveBeforeInstallPromptEvent);
 
-function logAppInstalled(evt) {
-  // CODELAB: Add code to log the event
-  console.log("Logistic App was installed.", evt);
-}
-
-// window.addEventListener("appinstalled", evt => {
-//   console.log("a2hs installed");
-// });
-
-// if (window.navigator.standalone === true) {
-//   console.log("display-mode is standalone");
+// function saveBeforeInstallPromptEvent(evt) {
+//   // CODELAB: Add code to save event & show the install button.
+//   deferredInstallPrompt = evt;
+//   installButton.removeAttribute("hidden");
 // }
 
-// const btnAdd = document.querySelector(".btnAdd");
+// function installPWA(evt) {
+//   if (deferredInstallPrompt !== undefined) {
+//     // CODELAB: Add code show install prompt & hide the install button.
+//     deferredInstallPrompt.prompt();
+//     // Hide the install button, it can't be called twice.
+//     evt.srcElement.setAttribute("hidden", true);
+//     // CODELAB: Log user response to prompt.
+//     deferredInstallPrompt.userChoice.then(choice => {
+//       if (choice.outcome === "accepted") {
+//         console.log("User accepted the A2HS prompt", choice);
+//       } else {
+//         console.log("User dismissed the A2HS prompt", choice);
+//       }
+//       deferredInstallPrompt = null;
+//     });
+//     deferredInstallPrompt.userChoice.then(choice => {
+//       if (choice.outcome === "accepted") {
+//         console.log("User accepted the A2HS prompt", choice);
+//       } else {
+//         console.log("User dismissed the A2HS prompt", choice);
+//       }
+//       deferredInstallPrompt = null;
+//     });
+//   }
+// }
 
-// btnAdd.addEventListener("click", e => {
-//   // hide our user interface that shows our A2HS button
-//   btnAdd.style.display = "none";
-//   // Show the prompt
-//   deferredPrompt.prompt();
-//   // Wait for the user to respond to the prompt
-//   deferredPrompt.userChoice.then(choiceResult => {
-//     if (choiceResult.outcome === "accepted") {
-//       console.log("User accepted the A2HS prompt");
-//     } else {
-//       console.log("User dismissed the A2HS prompt");
-//     }
-//     deferredPrompt = null;
-//   });
-// });
+// window.addEventListener("appinstalled", logAppInstalled);
+
+// function logAppInstalled(evt) {
+//   // CODELAB: Add code to log the event
+//   console.log("Logistic App was installed.", evt);
+// }
