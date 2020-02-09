cfe602875aff149e524bd98834c0fce165f890b8
pgswinter <tuannanh.nguyen.freelance@gmail.com>
2020-02-09 21:39:26 +0700

    4th

--- scripts/install.js
+++ scripts/install.js
@@ -1,6 +1,5 @@
 let deferredPrompt;
 const addBtn = document.querySelector('#btnAdd');
-addBtn.style.display = 'none';
 
 window.addEventListener('beforeinstallprompt', (e) => {
   // Prevent Chrome 67 and earlier from automatically showing the prompt
