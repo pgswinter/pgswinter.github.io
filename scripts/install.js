a25102e7a3cdfd15fe2e06da967ecc07ed987d83
pgswinter <tuannanh.nguyen.freelance@gmail.com>
2020-02-09 20:23:27 +0700

    4th

--- scripts/install.js
+++ scripts/install.js
@@ -1,20 +1,22 @@
 let deferredPrompt;
 
-const btnAdd = document.getElementById('btnAdd');
+const btnAdd = document.getElementById("btnAdd");
 
-window.addEventListener('beforeinstallprompt', (e) => {
-    e.preventDefault();
+window.addEventListener("beforeinstallprompt", e => {
+  e.preventDefault();
 
-    deferredPrompt = e;
-    btnAdd.style.display = 'block';
+  deferredPrompt = e;
+  btnAdd.style.display = "block";
 });
 
-btnAdd.addEventListener('click', (e) => {
+btnAdd.addEventListener("click", e => {
+  if (deferredPrompt) {
     deferredPrompt.prompt();
-    deferredPrompt.userChoice.then((choiceResult) => {
-        if(choiceResult.outcome === 'accepted') {
-            console.log('User accepted the A2HS prompt');
-        }
-        deferredPrompt = null;
-    })
-})
+    deferredPrompt.userChoice.then(choiceResult => {
+      if (choiceResult.outcome === "accepted") {
+        console.log("User accepted the A2HS prompt");
+      }
+      deferredPrompt = null;
+    });
+  }
+});
