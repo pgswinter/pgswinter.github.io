86913c36f41bf03ccd169cdcee23339049018eed
pgswinter <tuannanh.nguyen.freelance@gmail.com>
2020-02-09 21:11:32 +0700

    4th

--- scripts/install.js
+++ scripts/install.js
@@ -1,28 +1,28 @@
 let deferredPrompt;
+const addBtn = document.querySelector('#btnAdd');
+addBtn.style.display = 'none';
 
-const btnAdd = document.getElementById("btnAdd");
-
-window.addEventListener("beforeinstallprompt", e => {
+window.addEventListener('beforeinstallprompt', (e) => {
+  // Prevent Chrome 67 and earlier from automatically showing the prompt
   e.preventDefault();
-
+  // Stash the event so it can be triggered later.
   deferredPrompt = e;
-  btnAdd.style.display = "block";
-});
+  // Update UI to notify the user they can add to home screen
+  addBtn.style.display = 'block';
 
-btnAdd.addEventListener("click", e => {
-  if (deferredPrompt) {
+  addBtn.addEventListener('click', (e) => {
+    // hide our user interface that shows our A2HS button
+    addBtn.style.display = 'none';
+    // Show the prompt
     deferredPrompt.prompt();
-    deferredPrompt.userChoice.then(choiceResult => {
-      if (choiceResult.outcome === "accepted") {
-        console.log("User accepted the A2HS prompt");
-      } else {
-        console.log("User dismissed the A2HS prompt");
-      }
-      deferredPrompt = null;
-    });
-  }
-});
-
-window.addEventListener("appinstalled", evt => {
-  console.log("a2hs installed");
+    // Wait for the user to respond to the prompt
+    deferredPrompt.userChoice.then((choiceResult) => {
+        if (choiceResult.outcome === 'accepted') {
+          console.log('User accepted the A2HS prompt');
+        } else {
+          console.log('User dismissed the A2HS prompt');
+        }
+        deferredPrompt = null;
+      });
+  });
 });
