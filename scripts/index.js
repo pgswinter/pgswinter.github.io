88d4f7299bd9ce2dd9e4b808d0551aec6fba4856
pgswinter <tuannanh.nguyen.freelance@gmail.com>
2020-02-09 12:50:21 +0700

    5th

--- scripts/index.js
+++ scripts/index.js
@@ -8,25 +8,26 @@ window.addEventListener("beforeinstallprompt", e => {
 
 window.addEventListener("appinstalled", evt => {
   console.log("a2hs installed");
-  const btnAdd = document.querySelector(".btnAdd");
-
-  btnAdd.addEventListener("click", e => {
-    // hide our user interface that shows our A2HS button
-    btnAdd.style.display = "none";
-    // Show the prompt
-    deferredPrompt.prompt();
-    // Wait for the user to respond to the prompt
-    deferredPrompt.userChoice.then(choiceResult => {
-      if (choiceResult.outcome === "accepted") {
-        console.log("User accepted the A2HS prompt");
-      } else {
-        console.log("User dismissed the A2HS prompt");
-      }
-      deferredPrompt = null;
-    });
-  });
 });
 
 if (window.navigator.standalone === true) {
   console.log("display-mode is standalone");
 }
+
+const btnAdd = document.querySelector(".btnAdd");
+
+btnAdd.addEventListener("click", e => {
+  // hide our user interface that shows our A2HS button
+  btnAdd.style.display = "none";
+  // Show the prompt
+  deferredPrompt.prompt();
+  // Wait for the user to respond to the prompt
+  deferredPrompt.userChoice.then(choiceResult => {
+    if (choiceResult.outcome === "accepted") {
+      console.log("User accepted the A2HS prompt");
+    } else {
+      console.log("User dismissed the A2HS prompt");
+    }
+    deferredPrompt = null;
+  });
+});
