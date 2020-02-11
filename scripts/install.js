8018c3ef75d9925dcca4838b99ca44e66d55a634
tuananhnguyenogilvy <tuananh.nguyen@ogilvy.com>
2020-02-11 17:15:04 +0700

    update 7th

--- scripts/install.js
+++ scripts/install.js
@@ -40,15 +40,17 @@ function listenToUserAction() {
 
 // present install prompt to user
 function presentAddToHome() {
-  promptEvent.prompt();  // Wait for the user to respond to the prompt
-  promptEvent.userChoice
-    .then(choice => {
+  if (promptEvent) {
+    promptEvent.prompt();  // Wait for the user to respond to the prompt
+    promptEvent.userChoice
+      .then(choice => {
         if (choice.outcome === 'accepted') {
-            console.log('User accepted');
+          console.log('User accepted');
         } else {
-            console.log('User dismissed');
+          console.log('User dismissed');
         }
-    })
+      })
+  }
 }
 
 // window.addEventListener('beforeinstallprompt', (e) => {
