7feeefd7d3697f5502a8d3cc37e8d36a74da360a
tuananhnguyenogilvy <tuananh.nguyen@ogilvy.com>
2020-02-11 16:35:04 +0700

    update 7th

--- scripts/install.js
+++ scripts/install.js
@@ -9,10 +9,10 @@ window.addEventListener('beforeinstallprompt', (e) => {
   deferredPrompt = e;
   // Update UI to notify the user they can add to home screen
   // addBtn.style.display = 'block';
-
-  deferredPrompt.prompt();
-  // Wait for the user to respond to the prompt
-  deferredPrompt.userChoice.then((choiceResult) => {
+  if (deferredPrompt) {
+    deferredPrompt.prompt();
+    // Wait for the user to respond to the prompt
+    deferredPrompt.userChoice.then((choiceResult) => {
       if (choiceResult.outcome === 'accepted') {
         console.log('User accepted the A2HS prompt');
       } else {
@@ -20,11 +20,13 @@ window.addEventListener('beforeinstallprompt', (e) => {
       }
       deferredPrompt = null;
     });
+  }
+
   // addBtn.addEventListener('click', (e) => {
   //   // hide our user interface that shows our A2HS button
   //   addBtn.style.display = 'none';
   //   // Show the prompt
-   
+
   // });
 });
 
