64ec13d4b0a537406fd4b28f1ec0dde52d2412cc
tuananhnguyenogilvy <tuananh.nguyen@ogilvy.com>
2020-03-22 19:28:58 +0700

    initial commit workbox

---
+++ precache-manifest.69b3e8eb535ea69a2eb591d96b6a9835.js
@@ -0,0 +1,84 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81581dab8f39112ddf4fa90d9c862892",
+    "url": "/static/mcisg/images/Bitmap.81581dab8f39112ddf4f.png"
+  },
+  {
+    "revision": "8ca32b7b4afb8b1057107c34396c5264",
+    "url": "/static/mcisg/images/Image1.8ca32b7b4afb8b105710.png"
+  },
+  {
+    "revision": "6589033828bbdea6896a481b9655c606",
+    "url": "/static/mcisg/images/Image2.6589033828bbdea6896a.png"
+  },
+  {
+    "revision": "373955a074f50c03cdf88de02db5554c",
+    "url": "/static/mcisg/images/Image3_android.373955a074f50c03cdf8.png"
+  },
+  {
+    "revision": "4e35d05f51d6c8ca5964043c13569e6b",
+    "url": "/static/mcisg/images/Image_android.4e35d05f51d6c8ca5964.png"
+  },
+  {
+    "revision": "cfd55b11678740dabc1747341bafd582",
+    "url": "/static/mcisg/images/MSD_Logo_white.cfd55b11678740dabc17.svg"
+  },
+  {
+    "revision": "254f5952af49ade6c8abee8213c8903a",
+    "url": "/static/mcisg/images/login_screen.254f5952af49ade6c8ab.png"
+  },
+  {
+    "revision": "4df9aeb3f1cb9f087ec3d1c273444709",
+    "url": "/static/mcisg/images/tablet_login_screen.4df9aeb3f1cb9f087ec3.png"
+  },
+  {
+    "url": "app.314b8596fafd76c7ed23.js"
+  },
+  {
+    "revision": "20778f6887a7bb7ce661",
+    "url": "app.7c739ddb1bc7ae5b208e.css"
+  },
+  {
+    "revision": "e815531f3b82be55984d24eea498a0cb",
+    "url": "changepassword.html"
+  },
+  {
+    "revision": "45d644592179524307c44651106c8fb1",
+    "url": "editprofile.html"
+  },
+  {
+    "revision": "9f87150502680fb7de3bf62d5e1b97d5",
+    "url": "forgotpassword.html"
+  },
+  {
+    "revision": "7e6775789d6dce651b07602338efbf36",
+    "url": "index.html"
+  },
+  {
+    "revision": "73fdc415895a317d60ea4ded19c7c7ed",
+    "url": "legalacceptances.html"
+  },
+  {
+    "revision": "e876c72c60b172125bf4c830e6327fdf",
+    "url": "login.html"
+  },
+  {
+    "revision": "a6badb85f02ead8070b0b26359bb4ed5",
+    "url": "lung/index.html"
+  },
+  {
+    "revision": "733f09739af3009f51687fc8673c6997",
+    "url": "lung/onel-combination-therapy/index.html"
+  },
+  {
+    "revision": "78f978b2e5cb005ac4deae510d3cad90",
+    "url": "lung/onel-combination-therapy/study-design/index.html"
+  },
+  {
+    "revision": "02652bcf80b745101e90603063d8dbdb",
+    "url": "registration.html"
+  },
+  {
+    "url": "sw.314b8596fafd76c7ed23.js"
+  }
+]);
